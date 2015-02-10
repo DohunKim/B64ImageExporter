04ec6f1283b6ad508ca26338f1f4accdb091f3b1
Dohun Kim <dohun.kim@twinklelab.net>
2015-02-11 03:51:56 +0900

    entire solution commit

---
+++ B64ImgExporter/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ApplicationName("B64ImgExporter")]
+
+[assembly: Description("B64ImgExporter")]
+
+[assembly: ApplicationActivation(ActivationOption.Server)]
+
+[assembly: ApplicationAccessControl(false)]
+
+
+
+namespace B64ImgExporter
+{
+    public class B64ImgExporter : ServicedComponent
+    {
+        public string ExportToFile(string strData, string strFileName)
+        {
+            try
+            {
+                string[] arStr = strData.Split(',');
+
+                Console.WriteLine(arStr[1]);
+
+                byte[] data = System.Convert.FromBase64String(arStr[1]);
+
+                FileStream fs = System.IO.File.Create(strFileName +"."+ arStr[0].Split(';')[0].Replace("data:image/", ""));
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+
+                return arStr[0].Split(';')[0].Replace("data:image/", "");
+            } catch(Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
